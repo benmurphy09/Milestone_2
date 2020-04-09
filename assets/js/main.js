f953fbc6f3e9bd107034585007eff23bcc93350f
benmurphy09 <bmurphy@ohiochristian.edu>
2020-04-09 01:05:41 +0000

    added a slideshow header under the navbar and edited navbar slightly to improve preformance

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,3 +29,20 @@ window.onclick = function(event) {
     }
   }
 }
+
+var slideSet = 0;
+
+var showSlides = () => {
+  var slides = document.getElementsByClassName("mySlide")
+    for(let i=0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideSet++;
+      if(slideSet > slides.length) {
+        slideSet = 1;
+      }
+      slides[slideSet - 1].style.display = 'block';
+      setTimeout( showSlides , 5000);
+};
+
+showSlides();
